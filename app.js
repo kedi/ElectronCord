adc89b7687641fe02af7fda7250af11f3eb47270
Toasty <furkanozkan2002.fo@gmail.com>
2020-07-14 12:53:15 +0300

    Express is here... And some backend setup

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
-const url = require("url");
-const path = require("path");
 const electron = require("electron");
-const config = require("./config.json")
-
-const Discord = require("discord.js")
+const config = require("./config.json");
+const Discord = require("discord.js");
+const express = require("express");
+const bodyParser = require("body-parser")
 const client = new Discord.Client();
-
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
 
+const ExpressApp = express();
+
+ExpressApp.listen(3000);
+
+ExpressApp.set("view engine", "ejs");
+ExpressApp.use("/public", express.static("public"));
+ExpressApp.use(bodyParser.urlencoded({extended: true}))
+
+ExpressApp.get("/", async(req, res) => {
+
+    res.render("main", {client});
+
+});
+
+ExpressApp.post("/guilds/", async(req,res) => {
+
+    let guild = await client.guilds.get(req.body.guildID)
+    if(!guild) return res.render("main", {client});
+
+    return res.render("guildUI", {client, guild});
+    
+})
+
+
+// ExpressApp.post("/guilds", async(req,res) => {
+
+//     let guild = await client.guilds.get(req.body.guildID)
+//     if(!guild) return res.render("main", {client});
+    
+//     return res.render("guildUI", {client, guild});
+
+// })
+
 app.on("ready", () => {
 
     mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 1080,
         resizable: true,
         title: "Application",
         webPreferences: {
@@ -20,31 +53,32 @@ app.on("ready", () => {
         }
     });
 
-    /**
-     * We want to make sure that everything is up when we load a new url to our browser window.
-     */
     client.on("ready", () => {
-        mainWindow.loadURL(url.format({
-            pathname: path.join(__dirname, "public", "views", "main.html"),
-            protocol: "file",
-            slashes: true
-        }));
-    })
-    
-    mainWindow.on("closed", () => { app.quit() });
 
+        mainWindow.loadURL("http://localhost:3000/");
+        mainWindow.on("closed", () => {
+
+            app.quit();
+
+        });
+    
+    });
+
+    mainWindow.setMenu(null);
+    mainWindow.webContents.openDevTools()
 });
 
 /**
  * The ipcMain event handler of the ipcRenderer. Returns with an overview information about client.
  */
 ipcMain.on("get:DiscordClient", (event, arg) => {
+
     event.sender.send("return:DiscordClient", {
         guilds: client.guilds,
         user: client.user,
         users: client.users
-    })
+    });
+
 });
 
-
-client.login(config.DISCORD_TOKEN)
+client.login(config.DISCORD_TOKEN);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,14 @@
-const electron = require("electron");
-const { ipcRenderer } = electron;
+// const electron = require("electron");
+// const { ipcRenderer } = electron;
 
-ipcRenderer.send("get:DiscordClient", "hi")
-
-
-ipcRenderer.on("return:DiscordClient", (event, client) => {
-    document.querySelectorAll("p")[0].innerHTML = `Your bot is on ${client.guilds.size} guild and serving ${client.users.size} users!`
-})
+// ipcRenderer.send("get:DiscordClient", "hi")
 
 
+// ipcRenderer.on("return:DiscordClient", (event, client) => {
+//     document.querySelectorAll("p")[0].innerHTML = `Your bot is on ${client.guilds.size} guild and serving ${client.users.size} users!`
+// })
 
-;0
+
+
+// ;0
 
